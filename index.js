2ea7eabbe7695cda3f1c38d027e1c65d485837c3
AjeetKumar002 <ajeetsadhwani@gmail.com>
2023-07-12 21:07:47 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".auto-type",{
+    strings : ["Freelancer", "Photographer", "Programmer", "Chef", "Lawyer", "Fashion", "Personal","Journalist"],
+    typeSpeed : 150,
+    backSpeed : 150,
+    loop : true
+})
